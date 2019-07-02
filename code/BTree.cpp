4742e75daf727b499c3af597183f35dc5ef73f3a
siag7072 <siag7072@colorado.edu>
2019-07-02 19:51:45 +0000

    added class files

--- code/BTree.cpp
+++ code/BTree.cpp
@@ -9,3 +9,22 @@ BTree::BTree(){
 BTree::~BTree(){
 }
 
+void BTree::Insert(shared_ptr<btree> root, int key){
+
+}
+
+void BTree::Remove(shared_ptr<btree> root, int key){
+
+}
+
+shared_ptr<btree> BTree::Find(shared_ptr<btree> root, int key){
+
+}
+
+int BTree::CountNodes(shared_ptr<btree> root){
+
+}
+
+int BTree::CountKeys(shared_ptr<btree> root){
+
+}
